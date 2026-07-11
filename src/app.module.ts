eb78d5bcf21de28f8bc1ad7ff288640056b4b935
take44444 <take44444.general@gmail.com>
2026-07-12 01:20:16 +0900

    Support .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AgentsModule } from './agents/agents.module';
@@ -6,7 +7,12 @@ import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [PrismaModule, AuthModule, AgentsModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    PrismaModule,
+    AuthModule,
+    AgentsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
